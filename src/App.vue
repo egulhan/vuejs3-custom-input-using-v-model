886901e29438c8420af881c9931fff9b2ccc92f4
Erman <Glhan>
2022-05-21 13:42:02 +0300

    develop a multiple-choice input

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit.prevent="submitForm">
+    <div class="input-group">
+      <label>How are you feeling?</label>
+      <multiple-choice-input
+          v-model="selectedFeelingIndex"
+          :options="feelingOptions">
+      </multiple-choice-input>
+    </div>
+    <div class="input-group">
+      <button type="submit">Send</button>
+    </div>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MultipleChoiceInput from "@/components/MultipleChoiceInput";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {MultipleChoiceInput},
+  data() {
+    return {
+      selectedFeelingIndex: 0,
+      feelingOptions: ['Awesome', 'So so', 'Sad'],
+    };
+  },
+  methods: {
+    submitForm() {
+      alert(`You selected: ${this.feelingOptions[this.selectedFeelingIndex]}`);
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MultipleChoiceInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul>
+    <li v-for="(option, index) in options" :key="index" @click="changeOption(index)"
+        :class="{active: this.modelValue===index}">
+      {{ option }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    defaultOption: {
+      type: Number,
+      required: false,
+    },
+  },
+  emits: ['update:modelValue'],
+  name: "MultipleChoiceInput",
+  methods: {
+    changeOption(option) {
+      this.$emit('update:modelValue', option);
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  display: flex;
+}
+
+ul li {
+  display: flex;
+  margin: 0 2rem;
+  width: 4rem;
+  padding: 1rem;
+  justify-content: center;
+  background: white;
+  color: #111;
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
+
+ul li.active {
+  background: burlywood;
+  border: 1px solid darkgoldenrod;
+}
+</style>
